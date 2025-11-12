f8c13b53dc92dd0549c87c1a0ff17e2481956d36
mrnickcong <mrnickcong@gmail.com>
2025-11-12 10:16:43 +0800

    任务一：删除有序数组中的重复项

---
+++ basic-one/task-one/remove-duplicates/remove-duplicates.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+
+func main() {
+	fmt.Println(removeDuplicates([]int{1, 1, 2}))
+}
